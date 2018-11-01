1c5fe5e6b2aea2c13416e60116a365273a7c70e5
Jean Saunie <jean.saunie@devinci.fr>
2018-11-01 16:29:54 +0100

    Emitting events

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+let app = require( 'express' )();
+let http = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( http );
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-});
+app.get( '/', function (req, res) {
+    res.sendFile( __dirname + '/index.html' )
+} );
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-});
+io.on( 'connection', function (socket) {
+    console.log( 'a user connected' );
+    socket.on( 'disconnect', function () {
+        console.log( 'user disconnected' );
+    } );
+    socket.on( 'chat message', function (msg) {
+        console.log( 'message: ' + msg );
+    } );
+} );
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+http.listen( 3000, function () {
+    console.log( 'listening on *:3000' );
+} );
